501e325056e221aba7cc761db40ace6e0bb1743d
Fabien Hermenier <fabien.hermenier@nutanix.com>
2024-06-07 13:31:21 +0200

    relayout

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 VERSION=v1
-SRC_DIR=v1/protos
+SRC_DIR=protos/v1
 PKG=github.com/fhermeni/proto-cache/v1
-DST_DIR=v1/go
+DST_DIR=go/v1
 
 set -x
 protoc --go_out=${DST_DIR} --proto_path=${SRC_DIR} --go_opt=module=${PKG} ${SRC_DIR}/other/types.proto
